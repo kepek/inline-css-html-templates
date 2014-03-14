d2ae6031787c8a315128f67bb8021d94e736db2f
kepek <michal@kechner.name>
2014-03-14 17:01:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,95 @@
-inline-css-html-templates
-=========================
+## Inline CSS &amp; HTML Templates
 
-Inline CSS &amp; HTML Templates
+Every good Front-end developer know well that CSS created for Newsletter Template should be written inline for each HTML element.
+
+Not anymore! This simple tool will help you creating Newsletter Templates with popular technologies like: HTML, Twig, JSON &amp; LESS.
+
+- Yes, you can use all the benefits of Twig (includes, blocks, filters, etc!).
+- Yes, you can speed up your work by using the benefits of LESS!
+- Yes, you can define your content in JSON file for each template separately!
+- Yes, you can use "Debug" mode. It means: Clean code. It will help you Inspect DOM elements in Browsers.
+
+The thing is that you can finally split your LESS/CSS and HTML into separated files.
+
+Tool will automatically convert everything to one file.
+
+Yes! It's not a mistake! CSS will be added as inline styles to each element you specified in your Stylesheet.
+
+Sounds good, right? Try it! :-)
+
+TL;DR;
+----------
+
+Tool which will help you creating inline CSS &amp HTML templates much much quicker!
+
+Installing
+----------
+
+#### With Git
+Navigate to your Terminal.app and go to Sites directory:
+
+	cd ~/Sites/
+
+Create inline-css directory.
+
+	mkdir inline-css && cd inline-css
+
+And clone the repo to a new directory right then and there:
+
+	git clone git@github.com/kepek/inline-css-html-templates.git
+
+#### Manually
+* Download the .zip file from the GitHub downloads modal
+* Unzip the files and rename the folder to "inline-css"
+* Copy the folder to your Sites directory:
+	- `(~/Sites/)`
+
+Bugs &amp; Additions
+----------------
+
+Have a bug or want to add something? Please create an issue or a pull request right here.
+
+Usage
+------------
+
+Install all required [Composer](https://getcomposer.org/) packages.
+
+When you don't have Composer installed yet, do this:
+
+	curl -sS https://getcomposer.org/installer | php
+	php composer.phar install
+
+When you already have a Composer, do this:
+
+	composer install
+
+Visit your website (for example):
+
+	http://127.0.0.1/inline-css/
+
+Check our Basic template:
+
+	http://127.0.0.1/inline-css/templates/basic.html
+
+Check our Basic template in Debug mode:
+
+	http://127.0.0.1/inline-css/templates/basic.html?debug=true
+
+Template Structure
+------------
+
+	my-template.html
+	my-template.less
+	my-template.json
+
+Resources (Thanks!)
+----------
+* [getcomposer.org](https://getcomposer.org/)
+* [twig.sensiolabs.org](http://twig.sensiolabs.org/)
+* [github.com/zaininnari/html-minifier](https://github.com/zaininnari/html-minifier)
+* [github.com/tijsverkoyen/CssToInlineStyles](https://github.com/tijsverkoyen/CssToInlineStyles)
+* [leafo.net/lessphp](https://leafo.net/lessphp/)
+
+Contact
+------------
+[michal@kechner.name](mailto:michal@kechner.name)
---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+// Ussing CSS to Inline Styles
+use \TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+// Using HTMLMinify
+use \zz\Html\HTMLMinify;
+// Set Templates
+$templates = glob('templates/*.html');
+// Has Mod_Rewrite?
+$mod_rewrite = in_array('mod_rewrite', apache_get_modules());
+?>
+<?php if (!$_GET): ?>
+	<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>Inline CSS &amp; HTML Templates</title>
+		</head>
+		<body>
+			<h1>Inline CSS &amp; HTML Templates</h1>
+			<?php if (count($templates) > 0): ?>
+				<ol>
+					<?php foreach ($templates as $template): ?>
+						<li>
+							<a href="<?php echo ( $mod_rewrite ? 'templates/'. basename($template, '.html') . '.html' : '?' . http_build_query(array('template' => basename($template, '.html'))) ); ?>">
+								<?php echo basename($template); ?>
+							</a>
+						</li>
+					<?php endforeach; ?>
+				</ol>
+			<?php endif; ?>
+			<hr>
+			<p>
+				&copy; 2014 <a href="http://github.com/kepek">@kepek</a>
+			</p>
+		</body>
+	</html>
+<?php else: ?>
+<?php
+	// Set Debug Mode
+	$debug = isset($_GET['debug']) && $_GET['debug'] !== 'false' ? true : false;
+	// Set Path
+	$path = dirname(__FILE__) . '/templates/';
+	// Set Template
+	$template = isset($_GET['template']) ? $_GET['template'] . '.html' : false;
+	$template = file_exists($path . $template) ? $template : false;
+	// Autoload
+	require 'vendor/autoload.php';
+	// Set CSS to Inline Styles
+	$cssToInlineStyles = new CssToInlineStyles();
+	// Report All Errors
+	error_reporting(E_ALL);
+	// Display Errors
+	ini_set('display_errors', '1');
+	if ($template) {
+		// Set Twig Loader
+		$loader = new Twig_Loader_Filesystem($path);
+		// Set Twig
+		$twig = new Twig_Environment($loader);
+		// Set Filename
+		$filename = basename($template, '.html');
+		// Set LESS
+		$less = new lessc;
+		// Compile LESS
+		$less->checkedCompile(('templates/' . $filename . '.less'), ('templates/' . $filename . '.css'));
+		// Set JSON
+		$json = $filename . '.json';
+		// Set Stylesheet
+		$stylesheet = $filename . '.css';
+		// Set Data
+		$data = file_exists('templates/' . $json) ? json_decode(file_get_contents( dirname(__FILE__) . '/templates/' . $json  ), true) : array('data' => false);
+		// Add Debug Data
+		if ($debug) {
+			$data = array_merge($data, array('debug' => $debug));
+		}
+		// Add Some Template Data
+		$data = array_merge($data, array(
+			'filename' => $filename,
+			'stylesheet' => '/templates/' . $stylesheet
+		));
+		// Set HTML
+		$html = $twig->render($template, $data);
+		// Default Mode
+		if (!$debug) {
+			// Set HTML
+			$cssToInlineStyles->setHTML($html);
+			// Set CSS
+			$css = file_exists('templates/' . $stylesheet) ? file_get_contents('templates/' . $stylesheet) : null;
+			$cssToInlineStyles->setCSS($css);
+			// Render Template
+			$html = $cssToInlineStyles->convert();
+			// Minify HTML [OPTIMIZATION_SIMPLE, OPTIMIZATION_ADVANCED]
+			$HTMLMinify = new HTMLMinify($html, array('optimizationLevel' => HTMLMinify::OPTIMIZATION_SIMPLE));
+			// Output
+			echo $HTMLMinify->process();
+		}
+		// Debug Mode
+		else {
+			// Output
+			echo $html;
+		}
+	}
+?>
+<?php endif; ?>
